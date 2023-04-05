b8014f4da21310f20c96c356b5d2adb04f967689
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-04-05 18:06:25 -0500

    create_models

---
+++ src/models/userData.models.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+const db = require("../utils/database")
+
+
+const UserData = db.define('user_datas', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true,
+  },
+  address: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  phone: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true
+  },
+  avatar: {
+    type: DataTypes.TEXT,
+    unique: true
+  }
+});
+
+module.exports = UserData;
